b7cf3e734670be58108a898d27809abef2238786
parkhyunseo <dazzle1999@naver.com>
2025-03-17 20:15:46 +0900

    Feat: Ranking 드롭다운 퍼블리싱

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,19 +1,43 @@
 const icon_wrapper = document.querySelectorAll(".icon_wrapper");
 const default_select = document.getElementById("default_select");
+const rankerDropTop = document.querySelector(".ranker_drop_top");
+const rankerDropdown = document.getElementById("ranker_dropdown");
+const underArrow = document.querySelector(".under_arrow");
+const languageToggles = document.querySelectorAll(".language_drop_top");
 
-// 초기 선택 상태 설정
 default_select.classList.add("selected");
-
-// 각 이미지에 클릭 이벤트 리스너를 추가
 
 icon_wrapper.forEach((icon) => {
   icon.addEventListener("click", () => {
-    // 모든 이미지를 초기화
     icon_wrapper.forEach((el) => {
       el.classList.remove("selected");
     });
 
-    // 클릭된 이미지에 'selected' 클래스 추가
     icon.classList.add("selected");
+  });
+});
+
+rankerDropTop.addEventListener("click", function () {
+  rankerDropdown.classList.toggle("show");
+
+  if (rankerDropdown.classList.contains("show")) {
+    underArrow.style.transform = "rotate(0deg)";
+  } else {
+    underArrow.style.transform = "rotate(-90deg)";
+  }
+});
+
+languageToggles.forEach((toggle) => {
+  toggle.addEventListener("click", function () {
+    const subDropdown = this.nextElementSibling; // 바로 아래 있는 .sub_dropdown
+    const underArrow = this.querySelector(".under_arrow");
+
+    if (subDropdown.classList.contains("show")) {
+      subDropdown.classList.remove("show");
+      underArrow.style.transform = "rotate(-90deg)"; // 닫힐 때 오른쪽 방향
+    } else {
+      subDropdown.classList.add("show");
+      underArrow.style.transform = "rotate(0deg)"; // 열릴 때 아래 방향
+    }
   });
 });
