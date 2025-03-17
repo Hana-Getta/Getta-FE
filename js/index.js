30aec0519db259975b5e24c1fbd896b968cfb031
SonSuBin129 <ttokky0129@gmail.com>
2025-03-17 19:50:18 +0900

    feat: index 페이지 키값 저장 및 조회

--- js/index.js
+++ js/index.js
@@ -1,3 +1,47 @@
+$(document).ready(function () {
+  const users = localStorage.getItem("users");
+  const recordSection = $("#record_section");
+  const languages = ["C", "Python", "Java", "HTML"];
+
+  if (!users) {
+    const p = document.createElement("p");
+    p.innerText = "아직 기록이 없습니다 😭";
+    p.id = "no_record_section";
+    recordSection.append(p);
+  } else {
+    languages.forEach((lang) => {
+      const records = localStorage.getItem(lang);
+      if (records) {
+        const parsedRecords = JSON.parse(records);
+
+        if (parsedRecords.length > 0) {
+          const firstRecord = parsedRecords[0];
+
+          const div = document.createElement("div");
+          div.className = "record_item";
+
+          const p_lang = document.createElement("p");
+          p_lang.className = "record_language";
+          p_lang.innerText = firstRecord.language;
+
+          const p_nickname = document.createElement("p");
+          p_nickname.className = "record_nickname";
+          p_nickname.innerText = firstRecord.username;
+
+          const p_wpm = document.createElement("p");
+          p_wpm.innerText = `${firstRecord.wordsPerMinute} WPM`;
+
+          const p_accuracy = document.createElement("p");
+          p_accuracy.innerText = `${firstRecord.accuracy}%`;
+
+          div.append(p_lang, p_nickname, p_wpm, p_accuracy);
+          recordSection.append(div);
+        }
+      }
+    });
+  }
+});
+
 $("#go_record_btn").click(function () {
   window.location.href = "record.html";
 });
