8292bcc47602e4b4566fc35206ccccad8a08ed13
SonSuBin129 <ttokky0129@gmail.com>
2025-03-17 16:31:37 +0900

    feat: 닉네임 예외처리

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 $("#go_record_btn").click(function () {
   window.location.href = "record.html";
 });
+
+$("#nickname_submit").click(function (e) {
+  e.preventDefault();
+
+  const nicknameInput = $("#nickname_input").val().trim();
+  const errorMsg = $("#nickname_error");
+
+  if (!nicknameInput) {
+    errorMsg.css("visibility", "visible");
+  } else {
+    errorMsg.css("visibility", "hidden");
+    localStorage.setItem("nowUser", nicknameInput);
+    window.location.href = "language.html";
+  }
+});
