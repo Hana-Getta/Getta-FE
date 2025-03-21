364c203c87b5a1d238df3432980b1ad749919756
parkhyunseo <dazzle1999@naver.com>
2025-03-21 10:44:23 +0900

    Fix: 파일 경로 수정 및 example 파일 삭제

--- js/defaultComponent.js
+++ js/defaultComponent.js
@@ -1,36 +1,17 @@
-// $(function () {
-//   Promise.all([
-//     new Promise((resolve) =>
-//       $("#header").load("./components/header.html", resolve)
-//     ),
-//     new Promise((resolve) =>
-//       $("#sidebar").load("./components/sidebar.html", resolve)
-//     ),
-//   ]).then(() => {
-//     $("#loading").fadeOut();
-//   });
-// });
-
 // 경로 수정
 $(function () {
   let basePath = window.location.pathname.includes("/Getta-FE/")
-    ? "/Getta-FE/components/"
-    : "/components/";
+    ? "/Getta-FE/"
+    : "/";
 
   Promise.all([
     new Promise((resolve) =>
-      $("#header").load(basePath + "header.html", resolve)
+      $("#header").load(basePath + "./header.html", resolve)
     ),
     new Promise((resolve) =>
-      $("#sidebar").load(basePath + "sidebar.html", resolve)
+      $("#sidebar").load(basePath + "./sidebar.html", resolve)
     ),
   ]).then(() => {
     $("#loading").fadeOut();
   });
 });
-
-const cssPath = window.location.hostname.endsWith("github.io")
-  ? "/Getta-FE/css/header.css"
-  : "/css/header.css";
-
-document.getElementById("header_css").href = cssPath;
