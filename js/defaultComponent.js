c300528d7eea6100c64863a75aecdb9a8a02159e
parkhyunseo <dazzle1999@naver.com>
2025-03-21 10:20:19 +0900

    Fix: 경로 설정 수정

--- js/defaultComponent.js
+++ js/defaultComponent.js
@@ -1,10 +1,28 @@
+// $(function () {
+//   Promise.all([
+//     new Promise((resolve) =>
+//       $("#header").load("./components/header.html", resolve)
+//     ),
+//     new Promise((resolve) =>
+//       $("#sidebar").load("./components/sidebar.html", resolve)
+//     ),
+//   ]).then(() => {
+//     $("#loading").fadeOut();
+//   });
+// });
+
+// 경로 수정
 $(function () {
+  let basePath = window.location.pathname.includes("/Getta-FE/")
+    ? "/Getta-FE/components/"
+    : "/components/";
+
   Promise.all([
     new Promise((resolve) =>
-      $("#header").load("./components/header.html", resolve)
+      $("#header").load(basePath + "header.html", resolve)
     ),
     new Promise((resolve) =>
-      $("#sidebar").load("./components/sidebar.html", resolve)
+      $("#sidebar").load(basePath + "sidebar.html", resolve)
     ),
   ]).then(() => {
     $("#loading").fadeOut();
